331e8fada642dad920ad5f1bb04a141837bbb40f
Matheus Pimentel <matheuspimentel.pimentel@gmail.com>
2023-01-29 22:57:34 -0300

    Jogo do bloquinho até a aula 16

---
+++ Jogo do Bloquinho/Assets/Scripts/Bloco.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bloco : MonoBehaviour
+{
+    public GameObject somDoBloco;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        FindObjectOfType<GameManager>().AumentarQuantidadeDeBlocos();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Bola"))
+        {
+            Instantiate(somDoBloco, transform.position, transform.rotation);
+            FindObjectOfType<GameManager>().DiminuirQuantidadeDeBlocos();
+            Destroy(this.gameObject);
+            
+        }
+    }
+}
